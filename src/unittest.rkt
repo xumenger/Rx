0ff12817a272d8959cbe66d591ad115233dfcfbc
xumenger <xumenger@126.com>
2018-05-25 14:53:57 +0800

    rename assertequal to assert-equal

--- src/unittest.rkt
+++ src/unittest.rkt
@@ -1,10 +1,10 @@
 #lang racket
 
-(define (assertequal msg a b)
+(define (assert-equal msg a b)
   (if (eq? a b)
       (printf "correct: [~a] ~a == ~a" msg a b)
       (error 'wrong "[~a] ~a != ~a" msg a b))
   (newline))
 
 ; provide
-(provide assertequal)
+(provide assert-equal)
