3683b7fdf658731d9984647a17758f5f324c3b12
xumenger <xumenger@126.com>
2018-05-25 14:35:53 +0800

    testcase message

--- src/unittest.rkt
+++ src/unittest.rkt
@@ -1,9 +1,9 @@
 #lang racket
 
-(define (assertequal a b)
+(define (assertequal msg a b)
   (if (eq? a b)
-      (printf "correct!")
-      (error 'wrong! "~a != ~a" a b))
+      (printf "correct: [~a] ~a == ~a" msg a b)
+      (error 'wrong "[~a] ~a != ~a" msg a b))
   (newline))
 
 ; provide
